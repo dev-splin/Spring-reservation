662ad1c69226ddd47f57c3a07a8232d5171004af
dev-splin <dev.splin@gmail.com>
2021-04-28 21:36:00 +0900

    ADD : getDisplayInfos 완료(/displayinfos?categoryId=?start=?)

--- src/main/java/kr/or/connect/reservation/controller/api/DisplayinfoApiController.java
+++ src/main/java/kr/or/connect/reservation/controller/api/DisplayinfoApiController.java
@@ -1,17 +1,54 @@
 package kr.or.connect.reservation.controller.api;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.or.connect.reservation.dto.DisplayInfo;
 import kr.or.connect.reservation.service.CategoryService;
+import kr.or.connect.reservation.service.DisplayInfoService;
 
 @RestController
 @RequestMapping(path = "/api")
-public class DisplayinfoApliController {
+public class DisplayinfoApiController {
 	
 	@Autowired
 	CategoryService categoryService;
 	
+	@Autowired
+	DisplayInfoService displayInfoService;
+	
+	@GetMapping(path = "/displayinfos")
+	public Map<String, Object> getDisplayInfos(
+			@RequestParam(name = "categoryId", required = false, defaultValue = "0") int categoryId,
+			@RequestParam(name = "start", required = false, defaultValue = "0") int start) {
+		
+		int totalCount;
+		List<DisplayInfo> products;
+		
+		if(categoryId == 0) {
+			products = displayInfoService.getDisplayInfo(start);
+			totalCount = categoryService.getCount();
+		}
+		else {
+			products = displayInfoService.getDisplayInfoById(categoryId, start);
+			totalCount = categoryService.getCountById(categoryId);
+		}
+		
+		int productCount = products.size();
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("totalCount", totalCount);
+		map.put("productCount", productCount);
+		map.put("products", products);
+		
+		return map;
+	}
 	
 }
